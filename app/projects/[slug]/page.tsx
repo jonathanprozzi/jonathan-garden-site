bada193012008566181419faba6220a7c5c892ff
jonathanprozzi <jonathanprozzi@gmail.com>
2023-07-16 19:33:14 -0400

    wired up mdx with MDXContent. added project layout

---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,33 @@
+import { allProjects } from 'contentlayer/generated';
+import { useMDXComponent } from 'next-contentlayer/hooks';
+
+export const generateStaticParams = async () =>
+  allProjects.map((project) => ({ slug: project._raw.flattenedPath }));
+
+export const generateMetadata = ({ params }: { params: { slug: string } }) => {
+  const project = allProjects.find(
+    (project) => project._raw.flattenedPath === params.slug
+  );
+  if (!project) throw new Error(`Project not found for slug ${params.slug}`);
+  return { title: project.title };
+};
+
+const ProjectLayout = ({ params }: { params: { slug: string } }) => {
+  const project = allProjects.find(
+    (project) => project._raw.flattenedPath === params.slug
+  );
+  if (!project) throw new Error(`Project not found for slug ${params.slug}`);
+
+  const MDXContent = useMDXComponent(project.body.code);
+
+  return (
+    <section className="max-auto max-w-xl py-8">
+      <div className="mb-8 text-center">
+        <h1 className="text-2xl font-bold">{project.title}</h1>
+      </div>
+      <MDXContent />
+    </section>
+  );
+};
+
+export default ProjectLayout;
