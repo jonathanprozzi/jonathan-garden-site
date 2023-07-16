bdd6b687baae96060182f5fcb09d66c75752eee5
jonathanprozzi <jonathanprozzi@gmail.com>
2023-07-16 15:35:35 -0400

    added more to description on projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,10 +5,16 @@ export default function Projects() {
         <h1 className="text-bold text-xl md:text-3xl  bg-clip-text text-transparent bg-gradient-to-r from-teal-300 to-indigo-500">
           Projects
         </h1>
-        <div>
+        <div className="flex flex-col gap-2">
           <p>
-            I&apos;m a product engineer interested in learning,
-            decentralization, and tools for thought.
+            I have been fortunate to work on a variety of projects through the
+            years. Currently, I am most interested in projects relating to
+            decentralization, knowledge sharing, and democratizing access to
+            information.
+          </p>
+          <p>
+            I am in the process of updating this with current and previous
+            projects. My goal is to include case studies for featured projects.
           </p>
         </div>
       </div>
